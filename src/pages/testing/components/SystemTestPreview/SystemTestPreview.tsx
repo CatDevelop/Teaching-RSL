535b5bd5737f016a10440caf837234b864cb3913
Irina <tymanira@mail.ru>
2023-09-16 14:34:32 +0500

    Add UserTestPreview, fix SystemTestPreview

--- src/pages/testing/components/SystemTestPreview/SystemTestPreview.tsx
+++ src/pages/testing/components/SystemTestPreview/SystemTestPreview.tsx
@@ -8,14 +8,13 @@ type Props = Readonly<{
     name: string;
     allWordsCount: number;
     passedWordsCount: number;
-    onClick: () => void;
     className?: string;
 }>
 
-/** Плашка с описанием теста. */
+/** Плашка с описанием системного теста. */
 export const SystemTestPreview: FC<Props> = typedMemo(function SystemTestPreview(props){
     return (
-        <div className={clsx(styles.systemTestPreview, props.className)} onClick={props.onClick}>
+        <div className={clsx(styles.systemTestPreview, props.className)} onClick={() => {}}>
             <div className={styles.systemTestPreview__content}>
                 <p className={styles.systemTestPreview__name}>{props.name}</p>
                 <div className={styles.systemTestPreview__info}>
@@ -24,9 +23,9 @@ export const SystemTestPreview: FC<Props> = typedMemo(function SystemTestPreview
                     <p className={styles.systemTestPreview__description}>{props.passedWordsCount} из {props.allWordsCount} слов пройдено</p>
                 </div>
             </div>
-            <div className={styles.systemTestPreview__start}>
+            <button className={styles.systemTestPreview__start} onClick={() => {}} type="button">
                 <img src={Arrow} alt="Start test" className={styles.systemTestPreview__startIcon} />
-            </div>
+            </button>
         </div>
     );
 });
