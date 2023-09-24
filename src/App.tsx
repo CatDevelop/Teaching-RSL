0b0b040d7a70589b86d462687cf70130a0c4e195
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-09-25 00:57:53 +0500

    feat: add slider from mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import './App.css';
 import {HashRouter} from "react-router-dom";
-import { RootRouter } from "./routes/RootRouter";
+import {RootRouter} from "./routes/RootRouter";
+import {MantineProvider} from "@mantine/core";
+import '@mantine/core/styles.css';
 
 function MyApp() {
     return (
         <div className="App">
-            <HashRouter>
-                <RootRouter/>
-            </HashRouter>
+            <MantineProvider>
+                <HashRouter>
+                    <RootRouter/>
+                </HashRouter>
+            </MantineProvider>
         </div>
     );
 }
--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useCallback, useEffect, useRef } from "react";
-import { typedMemo } from "../../core/utils/typedMemo";
-import { ComponentProps } from "../../core/models/ComponentProps";
+import React, {FC, useCallback, useEffect, useRef} from "react";
+import {typedMemo} from "../../core/utils/typedMemo";
+import {ComponentProps} from "../../core/models/ComponentProps";
+import {Slider} from '@mantine/core';
 import styles from "./Range.module.css";
 import clsx from "clsx";
 
@@ -13,46 +14,58 @@ type Props = ComponentProps & Readonly<{
 
 /** Range input. */
 export const Range: FC<Props> = typedMemo(function Range({
-    onChange,
-    value,
-    min = 0,
-    max = 100,
-}){
+                                                             onChange,
+                                                             value,
+                                                             min = 0,
+                                                             max = 100,
+                                                         }) {
     const rangeRef = useRef<HTMLInputElement | null>(null);
 
     const handleRangeChange = useCallback((event: any) => {
-            let target = event.target
-            const min = target.min
-            const max = target.max
-            const val = target.value
-            let percentage = (val - min) * 100 / (max - min)
-            target.style.backgroundSize = percentage + '% 100%'
+        let target = event.target
+        const min = target.min
+        const max = target.max
+        const val = target.value
+        let percentage = (val - min) * 100 / (max - min)
+        target.style.backgroundSize = percentage + '% 100%'
     }, []);
 
     useEffect(() => {
-        if(rangeRef.current){
+        if (rangeRef.current) {
             rangeRef.current.addEventListener("input", handleRangeChange);
         }
     }, [rangeRef, handleRangeChange]);
 
     return (
+        // <div className={styles.range}>
+        //     <span className={clsx(styles.range__limit, styles.range__min)}>
+        //         {min}
+        //     </span>
+        //     <input
+        //         ref={rangeRef}
+        //         type="range"
+        //         min={min}
+        //         max={max}
+        //         value={value}
+        //         step="1"
+        //         className={styles.range__input}
+        //         onChange={onChange}
+        //     />
+        //     <span className={clsx(styles.range__limit, styles.range__max)}>
+        //         {max}
+        //     </span>
+        // </div>
         <div className={styles.range}>
-            <span className={clsx(styles.range__limit, styles.range__min)}>
-                {min}
-            </span>
-            <input
-                ref={rangeRef}
-                type="range"
-                min={min}
-                max={max}
-                value={value}
-                step="1"
-                className={styles.range__input}
-                onChange={onChange}
+            <Slider
+                color="blue"
+                min={10}
+                max={45}
+                marks={[
+                    {value: 10, label: '10'},
+                    {value: 27, label: '27'},
+                    {value: 45, label: '45'},
+                ]}
             />
-            <span className={clsx(styles.range__limit, styles.range__max)}>
-                {max}
-            </span>
         </div>
     );
 });
