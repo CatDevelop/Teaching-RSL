19078af24ddc2cce06193c3572c4eb77892f5375
Irina <tymanira@mail.ru>
2023-09-24 18:05:18 +0500

    Fix TestWordsModal

--- src/features/training/pages/TrainingCatalogPage/components/TestWordsModal/TestWordsModal.tsx
+++ src/features/training/pages/TrainingCatalogPage/components/TestWordsModal/TestWordsModal.tsx
@@ -1,17 +1,26 @@
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
 import { ComponentProps } from "../../../../../../core/models/ComponentProps";
 import { typedMemo } from "../../../../../../core/utils/typedMemo";
-import React,  { FC, ReactElement } from "react";
+import React,  { FC, ReactElement, useCallback, useState } from "react";
 import { Button } from "../../../../../../components/Button";
+import { Range } from "../../../../../../components/Range";
 
 type Props = ComponentProps & Readonly<{
     triggerComponent: (onOpen: () => void) => ReactElement;
 }>;
 
+const MIN_WORDS_COUNT = 10;
+const MAX_WORDS_COUNT = 45;
+
 export const TestWordsModal: FC<Props> = typedMemo(function TestWordsModal({
     triggerComponent
 }){
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const [wordsCount, setWordsCount] = useState(MIN_WORDS_COUNT);
+
+    const handleWordsChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setWordsCount(Number(event.target.value));
+    }, [])
 
     return (
         <>
@@ -22,7 +31,7 @@ export const TestWordsModal: FC<Props> = typedMemo(function TestWordsModal({
                     <>
                     <ModalHeader className="flex flex-col gap-1">Выберите количество слов</ModalHeader>
                     <ModalBody>
-                        body
+                        <Range min={MIN_WORDS_COUNT} max={MAX_WORDS_COUNT} value={wordsCount} onChange={handleWordsChange} />
                     </ModalBody>
                     <ModalFooter>
                         <Button variant="faded" onPress={onClose}>
