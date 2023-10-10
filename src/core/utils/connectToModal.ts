905abf1d588a6be0a6466996e1354a2f54fa4673
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-10-10 13:21:34 +0500

    change host

--- src/core/utils/connectToModal.ts
+++ src/core/utils/connectToModal.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io('wss://0.tcp.eu.ngrok.io:16168', {
+export const socket = io('ws://0.tcp.eu.ngrok.io:16168', {
     'reconnection': true,
     'reconnectionDelay': 500,
     'reconnectionAttempts': 10,
