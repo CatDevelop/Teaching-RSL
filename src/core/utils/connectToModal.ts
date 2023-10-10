3882d572db85f7c784e61050cb947b3fd8e876b5
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-10-10 13:31:25 +0500

    fix: some fixes

--- src/core/utils/connectToModal.ts
+++ src/core/utils/connectToModal.ts
@@ -4,6 +4,7 @@ export const socket = io('ws://0.tcp.eu.ngrok.io:16168', {
     'reconnection': true,
     'reconnectionDelay': 500,
     'reconnectionAttempts': 10,
+    'rejectUnauthorized': false,
     extraHeaders: {
         "ngrok-skip-browser-warning": "true"
     }
