3d63912199c1bdf25b6bd270e7f5206a0b42c93c
roma-cheby <105049918+roma-cheby@users.noreply.github.com>
2023-10-10 13:05:57 +0500

    change host ngrok

--- src/core/utils/connectToModal.ts
+++ src/core/utils/connectToModal.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io('ws://6.tcp.eu.ngrok.io:11616', {
+export const socket = io('wss://6.tcp.eu.ngrok.io:11616', {
     'reconnection': false,
     'reconnectionDelay': 500,
     'reconnectionAttempts': 10,
