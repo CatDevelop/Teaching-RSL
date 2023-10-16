ef4797b095ba90a3bb304080dfac927061699e10
Irina <tymanira@mail.ru>
2023-10-16 22:27:14 +0500

    Init RestorePasswordPage

---
+++ src/features/auth/pages/RestorePasswordPage/RestorePasswordPage.tsx
@@ -0,0 +1,8 @@
+import { typedMemo } from "core/utils/typedMemo";
+import { FC } from "react";
+
+export const RestorePasswordPage: FC<Props> = typedMemo(function RestorePasswordPage(props){
+    return (
+        
+    )
+})
