5c02cc3c680945944f6dc3b20ed2ff398436b228
roma-cheby <105049918+roma-cheby@users.noreply.github.com>
2023-10-22 22:06:49 +0500

    fix: Recognize core "ты" -> "Ты/тебя"

--- src/core/data.ts
+++ src/core/data.ts
@@ -46,7 +46,7 @@ export const StartThemeWords: Word[] = [
     {
         id: 4,
         text: "Тебя",
-        recognitionText: 'ты',
+        recognitionText: 'Ты/тебя',
         gifSource: YouSignVideo,
         // gifSource: "https://media.spreadthesign.com/video/mp4/12/12804.mp4",
         imageSource: You,
