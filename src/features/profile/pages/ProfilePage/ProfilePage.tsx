92613d90db68babc864d51317e15ef9bbf0bf661
CatDeveloper <maks.rozhkov.2004@gmail.com>
2023-10-30 03:23:35 +0500

    fix: change loader class

--- src/features/profile/pages/ProfilePage/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage/ProfilePage.tsx
@@ -14,7 +14,7 @@ export const ProfilePage: FC = typedMemo(function LearningCatalogPage() {
             <SideBar/>
             <PageContent className={styles.profile__pageContent}>
                 {/*<Suspense fallback={<Spinner className={styles.profile__loading}/>}>*/}
-                <Spinner className={styles.learningCatalog__loading}/>
+                <Spinner className={styles.profile__loading}/>
                 <Card className={styles.profile__levelBlock}>
                     <LevelBlock level={33} experience={1236} experienceForNextLevel={3000}/>
                 </Card>
