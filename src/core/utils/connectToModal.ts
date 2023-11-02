9fccacc14195bf44d3b44fd8280c1bb2ab272e58
roma-cheby <105049918+roma-cheby@users.noreply.github.com>
2023-11-02 14:31:31 +0500

    change host

--- src/core/utils/connectToModal.ts
+++ src/core/utils/connectToModal.ts
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io('wss://62.84.112.172', {
+export const socket = io('wss://62.84.112.172:5000', {
     'reconnection': true,
     'reconnectionDelay': 500,
     'reconnectionAttempts': 10,
