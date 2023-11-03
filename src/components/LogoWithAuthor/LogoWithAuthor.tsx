7349f2d3aa40fdbeb738790e5a21c155bd0e567a
Irina <tymanira@mail.ru>
2023-11-03 15:21:50 +0500

    Стили

---
+++ src/components/LogoWithAuthor/LogoWithAuthor.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+import {typedMemo} from "../../core/utils/typedMemo";
+import styles from "./LogoWithAuthor.module.css"
+
+export const LogoWithAuthor: FC<Props> = typedMemo(function LogoWithAuthor(props){
+    return (
+        <div  >
+
+        </div>
+    )
+})
