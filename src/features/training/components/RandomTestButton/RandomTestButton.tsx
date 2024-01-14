b8b84f519a88a420d5b2101d1009f44cd40acb40
Irina <tymanira@mail.ru>
2024-01-14 20:59:22 +0500

    feat - "Практика" - добавить кнопку создания рандомного теста

---
+++ src/features/training/components/RandomTestButton/RandomTestButton.tsx
@@ -0,0 +1,31 @@
+import {typedMemo} from "../../../../core/utils/typedMemo";
+import React, {FC, useCallback} from "react";
+import {ComponentProps} from "../../../../core/models/ComponentProps";
+import {Button} from "../../../../components/Button";
+import { useNavigate } from "react-router-dom";
+import { TrainingService } from "api/services/training";
+import { GetThemeResponse } from "core/models/themes/GetThemeListResponse";
+import { TestWordsModal } from "../TestWordsModal";
+import { TestTypeEnum } from "core/models/themes/TestTypeEnum";
+
+type Props = ComponentProps & Readonly<{
+}>
+
+const RANDOM_TEST_MAX_WORDS_COUNT = 50
+
+export const RandomTestButton: FC<Props> = typedMemo(function RecognitionBlock(props) {
+    const navigate = useNavigate();
+
+    const start = useCallback(async (wordsCount: GetThemeResponse['wordsCount'] ) => {
+        const test = await TrainingService.postTrainingCreate({wordsCount, trainingType: TestTypeEnum.RandomTest});
+        navigate(test.id)
+    }, [navigate])
+
+    const renderTestPreview = useCallback((onOpen: () => void) => {
+        return (
+            <Button color="primary" variant="light" onClick={onOpen}>Создать случайный тест</Button>
+        )
+    }, []);
+
+    return <TestWordsModal triggerComponent={renderTestPreview} start={start} maxWordsCount={RANDOM_TEST_MAX_WORDS_COUNT}/>;
+});
