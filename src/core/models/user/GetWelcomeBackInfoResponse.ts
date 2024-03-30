ab106e60d390952c6c2c2ddce51b8f438dcb9b97
Irina <tymanira@mail.ru>
2024-03-30 17:22:05 +0500

    fix: fix after user api changing

--- src/core/models/user/GetWelcomeBackInfoResponse.ts
+++ src/core/models/user/GetWelcomeBackInfoResponse.ts
@@ -6,39 +6,67 @@ export class GetWelcomeBackInfoResponse {
      * Имя
      */
     public readonly firstName: string | null;
-    
+
     /**
      * Фамилия
      */
     public readonly lastName: string | null;
-    
+
     /**
      * Почта
      */
     public readonly email: string | null;
-    
+
     /**
      * Текущий уровень
      */
     public readonly level: number;
-    
+
     /**
-     * Количество опыта
+     * Прогресс по практикам
      */
-    public readonly scoresCount: number;
+    public readonly progressCountTraining: number;
+
+    /**
+     * Прогресс по обучению
+     */
+    public readonly progressCountLearning: number;
+
+    /**
+     * Общий прогресс
+     */
+    public readonly progressCountAll: number;
+
+    /**
+     * Максимальный опыт в текущем уровне
+     */
+    public readonly maxLevelExperience: number;
+
+    /**
+     * Опыт пользователя
+     */
+    public readonly userExperience: number;
 
     public constructor({
         firstName,
         lastName,
         email,
         level,
-        scoresCount,
+        progressCountLearning,
+        progressCountTraining,
+        progressCountAll,
+        maxLevelExperience,
+        userExperience
     }: GetWelcomeBackInfoResponseProps) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.level = level;
-        this.scoresCount = scoresCount;
+        this.progressCountLearning = progressCountLearning;
+        this.progressCountTraining = progressCountTraining;
+        this.progressCountAll = progressCountAll;
+        this.maxLevelExperience = maxLevelExperience;
+        this.userExperience = userExperience;
     }
 }
 
