10a78a60f0f8af6d6bad8cec06acdb0ffede7038
Irina <tymanira@mail.ru>
2024-03-31 23:14:58 +0500

    feat: profile -> learning progress -> practice - add progress bar

--- src/features/profile/pages/ProfilePage/LearningProgress/TestProgress/TestProgress.tsx
+++ src/features/profile/pages/ProfilePage/LearningProgress/TestProgress/TestProgress.tsx
@@ -1,30 +1,48 @@
-import { typedMemo } from "core/utils/typedMemo";
-import React, { FC, useMemo } from "react";
+import {typedMemo} from "core/utils/typedMemo";
+import React, {FC, useMemo} from "react";
 import styles from "./TestProgress.module.css";
-import { Typography } from "components/Typography";
-import { UserTestHistoryRecordResponse } from "core/models/user/UserTestHistoryRecordResponse";
+import {Typography} from "components/Typography";
+import {UserTestHistoryRecordResponse} from "core/models/user/UserTestHistoryRecordResponse";
 import dayjs from 'dayjs'
+import {CircularProgress} from "@nextui-org/react";
 
 type Props = UserTestHistoryRecordResponse
 
 /**
  * Прогресс выполнененного теста
  */
-export const TestProgress: FC<Props> = typedMemo(function TestProgress(props){
+export const TestProgress: FC<Props> = typedMemo(function TestProgress(props) {
     const completedDate = useMemo(() => dayjs(props.completedDateTime), [props.completedDateTime]);
 
     return (
         <div className={styles.testProgress}>
-            <div className={styles.testProgress__header}>
-                <Typography variant="p" className={styles.testProgress__name}>{props.name}</Typography>
-                <Typography variant="span" className={styles.testProgress__progress}>
-                    {props.wordsCompletedCount}/{props.wordsCount}
+            <div className={styles.testProgress__info}>
+                <Typography variant="p" className={styles.testProgress__name}>
+                    {props.name}
+
+                    <Typography variant="span" className={styles.testProgress__progress}>
+                        {props.wordsCompletedCount}/{props.wordsCount}
+                    </Typography>
                 </Typography>
+                <div className={styles.testProgress__dates}>
+                    <Typography variant="span"
+                                className={styles.testProgress__time}>{completedDate.format('HH:mm')}</Typography>
+                    <Typography variant="span"
+                                className={styles.testProgress__date}>{completedDate.format('DD.MM.YYYY')}</Typography>
+                </div>
             </div>
-            <div className={styles.testProgress__dates}>
-                <Typography variant="span" className={styles.testProgress__time}>{completedDate.format('HH:mm')}</Typography>
-                <Typography variant="span" className={styles.testProgress__date}>{completedDate.format('DD.MM.YYYY')}</Typography>
-            </div>
+
+            <CircularProgress
+                size="md"
+                value={(props.wordsCompletedCount / props.wordsCount) * 100}
+                color="secondary"
+                showValueLabel
+                classNames={{
+                    svg: styles.testProgress__chartSvg,
+                    indicator: styles.testProgress__chartIndicator,
+                    value: styles.testProgress__chartValue,
+                }}
+            />
         </div>
     )
 })
