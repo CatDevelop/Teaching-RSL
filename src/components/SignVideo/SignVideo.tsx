4d8201b6e8c1f05d1921e63eb2d80d3f9185352d
Irina <tymanira@mail.ru>
2024-04-21 23:44:49 +0500

    feat: implement in sign video

--- src/components/SignVideo/SignVideo.tsx
+++ src/components/SignVideo/SignVideo.tsx
@@ -4,6 +4,8 @@ import {ComponentProps} from "../../core/models/ComponentProps";
 import styles from "./SignVideo.module.css";
 import clsx from "clsx";
 import {Spinner} from "@nextui-org/react";
+import {Video} from "../Video/Video";
+import {MaterialModal} from "../MaterialModal/MaterialModal";
 
 type Props = ComponentProps & Readonly<{
     src: string | null;
@@ -31,13 +33,25 @@ export const SignVideo: FC<Props> = typedMemo(function SignVideo(props) {
                 isLoading &&
                 <Spinner className={styles.loader}/>
             }
-            <video
+            <MaterialModal
                 className={clsx(styles.signVideo__video, isLoading && styles.signVideo__video_hide)}
-                src={props.src}
-                autoPlay
-                loop
-                muted
-                onCanPlayThrough={handleLoadVideo}
+                material={<Video
+                    src={props.src}
+                    autoPlay
+                    loop
+                    muted
+                    onCanPlayThrough={handleLoadVideo}
+                />}
+                expandedMaterial={
+                    <Video
+                        src={props.src}
+                        autoPlay
+                        loop
+                        muted
+                        controls
+                        onCanPlayThrough={handleLoadVideo}
+                    />
+                }
             />
         </div>
     )
