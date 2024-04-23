efe1dc0f9bb82645b2958ea546b8352c6f6e0237
CatDeveloper <maks.rozhkov.2004@gmail.com>
2024-04-23 23:26:48 +0500

    feat: add feautures

--- src/features/home/pages/HomePage/HomePage.tsx
+++ src/features/home/pages/HomePage/HomePage.tsx
@@ -1,36 +1,45 @@
-import React, {FC, useCallback, useEffect, useRef, useState} from "react";
+import React, {FC, useCallback, useEffect, useState} from "react";
 import styles from "./HomePage.module.css";
 import {useNavigate} from "react-router-dom";
 import {typedMemo} from "../../../../core/utils/typedMemo";
 import {ReactComponent as Logo} from "../../../../assets/images/LogoMonochrome.svg";
+import {ReactComponent as LogoShort} from "../../../../assets/images/LogoShort.svg";
 import {ReactComponent as HomePageHero} from "../../../../assets/images/HomePageHero.svg";
 import {Button} from "../../../../components/Button";
 import {Facts} from "../../components/facts";
 import {Typography} from "../../../../components/Typography";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../../store/store";
-import {ReactComponent as Test1} from "../../../../assets/images/Test1.svg";
-import { useWindowScroll } from '@mantine/hooks';
+import {useInViewport, useWindowScroll} from '@mantine/hooks';
 import {useRive, useStateMachineInput} from "@rive-app/react-canvas";
 import clsx from "clsx";
+import {UserSecretService} from "../../../../api/services/userSecret";
+import {login} from "../../../../store/auth/authSlice";
 
 /**
  * Главная страница (Лендинг)
  */
 export const HomePage: FC = typedMemo(function HomePage() {
+    const dispatch = useDispatch()
     const navigate = useNavigate();
     const isAuth = useSelector((state: RootState) => state.auth.isAuth)
 
-    const r = useRef(null)
-    const r2 = useRef(null)
+    const {ref: featureTitleRef1, inViewport: featureTitle1InViewport} = useInViewport();
+    const {ref: featureRef1, inViewport: feature1InViewport} = useInViewport();
+    const {ref: featureTitleRef2, inViewport: featureTitle2InViewport} = useInViewport();
+    const {ref: featureRef2, inViewport: feature2InViewport} = useInViewport();
+    const {ref: featureTitleRef3, inViewport: featureTitle3InViewport} = useInViewport();
+    const {ref: featureRef3, inViewport: feature3InViewport} = useInViewport();
+
     const [scroll, scrollTo] = useWindowScroll();
     const [isFixed, setIsFixed] = useState(false)
+    const [isTop, setIsTop] = useState(true)
 
     const toSignup = useCallback(() => navigate("/signup"), [])
     const toSignin = useCallback(() => navigate("/signin"), [])
     const toLearning = useCallback(() => navigate("/learning"), [])
 
-    const { rive, RiveComponent } = useRive({
+    const {rive, RiveComponent} = useRive({
         src: './Test.riv',
         stateMachines: "Base",
         autoplay: true,
@@ -39,32 +48,53 @@ export const HomePage: FC = typedMemo(function HomePage() {
     const levelInput = useStateMachineInput(rive, "Base", "Type");
 
     useEffect(() => {
+        if (UserSecretService.hasToken())
+            dispatch(login());
         // @ts-ignore
-        if(levelInput)
+        if (levelInput)
             levelInput.value = 0
     }, []);
 
 
     useEffect(() => {
         // @ts-ignore
-        const parent = r.current && r.current.getBoundingClientRect();
+        const featureTitle1 = featureTitleRef1.current && featureTitleRef1.current.getBoundingClientRect();
+        const feature1 = featureRef1.current && featureRef1.current.getBoundingClientRect();
         // @ts-ignore
-        const parent2 = r2.current && r2.current.getBoundingClientRect();
+        const featureTitle2 = featureTitleRef2.current && featureTitleRef2.current.getBoundingClientRect();
         // @ts-ignore
-        if (parent?.top <= 60 && parent2?.top >= 150) {
+        const featureTitle3 = featureTitleRef3.current && featureTitleRef3.current.getBoundingClientRect();
+        const feature3 = featureRef3.current && featureRef3.current.getBoundingClientRect();
+
+        // @ts-ignore
+        if (feature1?.top <= 90 && feature3?.top >= 60) {
             setIsFixed(true)
-            console.log(levelInput)
-            if(levelInput)
-                levelInput.value = 1
-            console.log(123)
+            if (levelInput)
+                levelInput.value = 0
         } else {
             setIsFixed(false)
-            if(levelInput)
+            if (levelInput)
                 levelInput.value = 0
         }
         // @ts-ignore
-        if(parent?.top > 60)
+        if (feature1?.top > 90)
             setIsFixed(false)
+
+        if (feature3?.top <= 60)
+            setIsTop(false)
+        else
+            setIsTop(true)
+
+
+        if (featureTitle1InViewport)
+            // @ts-ignore
+            levelInput.value = 0
+        else if (featureTitle2InViewport)
+            // @ts-ignore
+            levelInput.value = 1
+        else if (featureTitle3InViewport)
+            // @ts-ignore
+            levelInput.value = 2
 
     }, [scroll]);
 
@@ -82,7 +112,8 @@ export const HomePage: FC = typedMemo(function HomePage() {
                             </Button>
                         }
                         {!isAuth && <>
-                            <Button variant="light" color="primary" className={styles.header__buttons__registration} onClick={toSignup}>
+                            <Button variant="light" color="primary" className={styles.header__buttons__registration}
+                                    onClick={toSignup}>
                                 Зарегистрироваться
                             </Button>
                             <Button variant="solid" color="primary" onClick={toSignin}>
@@ -115,41 +146,63 @@ export const HomePage: FC = typedMemo(function HomePage() {
                 <Facts className={styles.homePage__facts}/>
             </div>
 
+            <Typography variant="h1" className={styles.homePage__features__title}>
+                Наши возможности
+            </Typography>
             <div className={styles.homePage__featuresContainer}>
-
                 <div className={styles.homePage__features__items}>
-                    <div className={styles.homePage__features__item} ref={r}>
+                    <div className={styles.homePage__features__item} ref={featureRef1}>
                         <div className={styles.homePage__features__item__text}>
-                            <Typography variant="h2" className={styles.homePage__features__item__text__title}>
+                            <Typography variant="h2" className={styles.homePage__features__item__text__title}
+                                        ref={featureTitleRef1}>
                                 Изучай теорию
                             </Typography>
                             <Typography variant="p" className={styles.homePage__features__item__text__description}>
-                                Изучай теорию
+                                Смотри, как показывают жесты профессиональные сурдопереводчики и запоминай их
                             </Typography>
-                        </div>
-                        <div className={styles.homePage__features__item__image}>
-                            <div className={styles.homePage__features__item__imageRive}>
-                                <RiveComponent className={clsx(styles.homePage__features__rive, isFixed && styles.homePage__features__rive_fixed)}/>
-                            </div>
-                            {/*<Test1 width={900} style={{*/}
-                            {/*    position: isFixed ? "fixed" : undefined, top: "50px", right: "118px"}}/>*/}
                         </div>
                     </div>
 
-                    <div className={styles.homePage__features__item} ref={r2}>
+                    <div className={styles.homePage__features__item} ref={featureRef2}>
                         <div className={styles.homePage__features__item__text}>
-                            <Typography variant="h2" className={styles.homePage__features__item__text__title}>
-                                Практикуйся
+                            <Typography variant="h2" className={styles.homePage__features__item__text__title}
+                                        ref={featureTitleRef2}>
+                                Закрепляй
                             </Typography>
-                            <Typography variant="p">
-                                Изучай теорию
+                            <Typography variant="p" className={styles.homePage__features__item__text__description}>
+                                Проходи тесты разных типов для закрепления материала
                             </Typography>
                         </div>
-                        <div className={styles.homePage__features__item__image}>
-                            <Test1 />
+                    </div>
+                    <div className={styles.homePage__features__item} ref={featureRef3}>
+                        <div className={styles.homePage__features__item__text}>
+                            <Typography variant="h2" className={styles.homePage__features__item__text__title}
+                                        ref={featureTitleRef3}>
+                                Практикуйся
+                            </Typography>
+                            <Typography variant="p" className={styles.homePage__features__item__text__description}>
+                                Показывай жесты в камеру - наша система распознает их и даст обратную связь
+                            </Typography>
                         </div>
                     </div>
                 </div>
+                <div className={styles.homePage__features__item__image} style={{alignItems: isTop ? "start" : "end"}}>
+                    <div className={styles.homePage__features__item__imageRive}>
+                        <RiveComponent
+                            className={clsx(styles.homePage__features__rive, isFixed && styles.homePage__features__rive_fixed)}/>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.end}>
+                <LogoShort width={100} className={styles.end__logo}/>
+                <Typography variant="p" className={styles.end__description}>
+                    Дарим свободу выражения <br/> в каждом жесте
+                </Typography>
+
+                <Button color="primary" className={styles.end__button} onClick={toLearning}>
+                    Начать изучение
+                </Button>
             </div>
         </div>
     )
